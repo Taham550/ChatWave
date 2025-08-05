43f755d3698f813004cbaccd32074bcaed63007b
Taham550 <tahammalik216@gmail.com>
2025-08-05 21:21:31 +0530

    Fix: use eventlet server for production

--- app.py
+++ app.py
@@ -145,4 +145,5 @@ def on_get_rooms():
     emit('rooms_list', rooms_info)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    
+    socketio.run(app, host='0.0.0.0', port=5000)
